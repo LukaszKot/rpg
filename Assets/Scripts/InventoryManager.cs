0a37336e919fc23b01305f42c82cb796867d5758
LukaszKOT <lukaszkot03@gmail.com>
2019-04-27 12:49:08 +0200

    done something

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour {
+
+	public static InventoryManager instance;
+	private List<Item> itemsList = new List<Item>();
+
+	private void Awake () {
+		if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+    }
+	
+	
+	private bool AddItem(Item item)
+	{
+		if (itemsList.Count >= 3) return false;
+		itemsList.Add(item);
+
+		return true;
+	}
+
+
+}
+
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item {
+	public string ItemName { get; private set; }
+	public Sprite ItemIcon { get; private set; }
+	
+	public Item(string itemName, Sprite itemIcon)
+	{
+		ItemName = itemName;
+		ItemIcon = itemIcon;
+	}
+}
